8ad8579adc7c640e0696f305f46407d4aeee730d
Mulyar Vlad <mulyarvlad@MulyarVlad.local>
2023-03-05 15:31:26 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+class Person {
+
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+  
+    personInfo(Person) {
+      console.log(this.Person);
+    }
+  }
+  
+class Car {
+    static owner = "John";
+  
+    constructor(carBrand, model, yearOfDischarge, licensePlate) {
+      this.carBrand = carBrand;
+      this.model = model;
+      this.yearOfDischarge = yearOfDischarge;
+      this.licensePlate = licensePlate;
+    }
+
+
+  }  
+  
